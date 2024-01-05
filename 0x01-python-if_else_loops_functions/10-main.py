521a41190ca33e7df3b83f277b5d9261dff4336f
verndinski <zainazayuni@gmail.com>
2024-01-05 08:17:53 -0800

    10-add.py

--- 0x01-python-if_else_loops_functions/10-main.py
+++ 0x01-python-if_else_loops_functions/10-main.py
@@ -4,3 +4,9 @@ add = __import__('10-add').add
 print(add(1, 2))
 print(add(98, 0))
 print(add(100, -2))
+#!/usr/bin/env python3
+add = __import__('10-add').add
+
+print(add(1, 2))
+print(add(98, 0))
+print(add(100, -2))
