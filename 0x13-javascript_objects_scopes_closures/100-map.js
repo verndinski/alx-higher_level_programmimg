5e28da6116c287ed56162d846f7f3b1f552b26cf
verndinski <zainazayuni@gmail.com>
2024-08-05 11:55:54 +0000

    100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,11 @@
 #!/usr/bin/node
-const list = require('./100-data.js').list;
-console.log(list);
-console.log(list.map((item, index) => item * index));
+
+// Import the array from 100-data.js
+const list = require('./100-data').list;
+
+// Compute a new array with each value multiplied by its index
+const newList = list.map((value, index) => value * index);
+
+// Print both the initial list and the new list
+console.log('Initial list:', list);
+console.log('New list:', newList);
